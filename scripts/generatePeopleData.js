141861c2f5b3504ac1b5f52a34f3f2903d7cc26f
Fil-Naj <filip.najdovski@outlook.com>
2025-09-20 23:46:26 +1000

    fix build generate people script

--- scripts/generatePeopleData.js
+++ scripts/generatePeopleData.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
 // Path to your public/people directory
 const peopleDir = path.join(process.cwd(), "public/people");
